86f47dd8da0401913e2ada8a4f0ad390d8175dcc
Ivan Kryvoruchko <gildraug@gmail.com>
2018-03-04 13:59:51 +0200

    Initial commit

---
+++ test/eftest_profiles/core_test.clj
@@ -0,0 +1,7 @@
+(ns eftest-profiles.core-test
+  (:require [clojure.test :refer :all]
+            [eftest-profiles.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
